7d1fd21ec32c65b51252c80b663d92538c05a175
maksimvolkov <mvolkov@aspirity.com>
2023-05-16 10:55:27 +0700

    init(repo): storybook
    Init project and add storybook

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import dts from 'vite-plugin-dts';
+import path from "path";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.tsx'),
+      name: '@easydev',
+      formats: ['es', 'umd'],
+      fileName: (format) => `easydev.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
