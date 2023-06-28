c18ad406beeae29036b9d9c063dafe8aa5a985a0
Kraysik <akrays@aspirity.com>
2023-06-28 12:40:24 +0700

    title

--- src/core/Typography/Title.tsx
+++ src/core/Typography/Title.tsx
@@ -1,5 +1,5 @@
-import { CSSProperties, ReactNode } from "react";
-import { StyledTitle, type TitleType, type TitleVariant } from "./styles.ts";
+import { CSSProperties, ReactNode } from 'react';
+import { StyledTitle, type TitleType, type TitleVariant } from './styles.ts';
 
 type TitleProps = {
   children: ReactNode | string | ReactNode[];
@@ -7,18 +7,12 @@ type TitleProps = {
   className?: string;
   style?: CSSProperties;
   type?: TitleType;
-}
+};
 
 export const Title = ({ children, variant, className, type, ...props }: TitleProps) => {
   return (
-    <StyledTitle
-      tag={variant || "h1"}
-      className={className}
-      type={type}
-      variant= {variant}
-      {...props}
-    >
+    <StyledTitle tag={variant || 'h1'} className={className} type={type} variant={variant} {...props} style={{color: 'red', textTransform: 'uppercase', font: 'serif'}}>
       {children}
     </StyledTitle>
-  )
-}
+  );
+};
