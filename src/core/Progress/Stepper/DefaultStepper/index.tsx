8452157a14afbba4f57dbf369cefed1087ea4bf4
Kraysik <akrays@aspirity.com>
2023-07-21 12:25:22 +0700

    EASY-1076 update default stepper

--- src/core/Progress/Stepper/DefaultStepper/index.tsx
+++ src/core/Progress/Stepper/DefaultStepper/index.tsx
@@ -3,10 +3,13 @@ import { StyledStepper } from '../styles.ts';
 import { StepperPropsType } from '../types.ts';
 
 export const DefaultStepper = ({ count, currentStep }: StepperPropsType) => {
+  const maxStep = currentStep > count ? count : currentStep;
+  const formattedCurrenStep = currentStep < 1 ? 1 : maxStep;
+
   return (
     <StyledStepper>
       {Array.from(Array(count).keys()).map((step) => (
-        <Step key={step} className={step < currentStep ? 'active' : ''} />
+        <Step key={step} className={step < formattedCurrenStep ? 'active' : ''} />
       ))}
     </StyledStepper>
   );
