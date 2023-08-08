23888ef867a7098dee2f4d2d3cd01db9da4710b1
Darya <dlukina@aspirity.com>
2023-08-08 15:01:51 +0700

    added documentation folder

---
+++ documentation/astro.config.mjs
@@ -0,0 +1,31 @@
+/* eslint-disable import/order */
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Easydev',
+			social: {
+				discord: 'https://discord.com/invite/8aJkBc8gFN'
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+
+	// Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+	image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+});
