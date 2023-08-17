a0c114ecdfe8adc0c52fc2a0ffb7fc9c48b21cc2
kraysik <akrays@aspirity.com>
2023-08-17 14:54:48 +0700

    remove ControlWithLabel export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { Title, Subtitle, Text } from '@core/Typography';
 export { Button } from '@core/Button';
 export { Input, Textarea, MaskedInput } from '@core/Inputs';
-export { Radio, ControlWithLabel, Toggle, Checkbox } from '@core/Controls';
+export { Radio, Toggle, Checkbox } from '@core/Controls';
 export { Space } from '@core/Space';
 export { Link } from '@core/Link';
 export * from '@core/Progress';
