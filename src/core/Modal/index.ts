eb62078859fe40afc812eaf761101b10f0ad41dd
Slava Dmitriev <vdmitriev@aspirity.com>
2023-08-23 13:00:56 +0700

    add exports

--- src/core/Modal/index.ts
+++ src/core/Modal/index.ts
@@ -1,1 +1,5 @@
 export { Modal } from './Modal';
+export { CloseButton } from './components/CloseButton';
+export { ModalHeader } from './components/ModalHeader';
+export { ModalBody } from './components/ModalBody';
+export { ModalFooter } from './components/ModalFooter';
