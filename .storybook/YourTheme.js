203fd85d223a2b28b68b5b86c861734393fffcf0
kraysik <akrays@aspirity.com>
2023-08-24 16:31:52 +0700

    upgrade storybook
    added favicon and storybook sidebar image

---
+++ .storybook/YourTheme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  brandTitle: 'Easydev',
+  brandUrl: '/',
+  brandImage: '/logo_dark.svg',
+  brandTarget: '_self',
+});
