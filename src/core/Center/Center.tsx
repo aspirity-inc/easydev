34d8b2a4d561e3d733a062139e5058855cffac7f
Pisklova Marina <pisklovama@gmail.com>
2023-08-30 09:30:31 +0500

    EASY-1105 Added gap prop

--- src/core/Center/Center.tsx
+++ src/core/Center/Center.tsx
@@ -3,12 +3,13 @@ import { ComponentPropsWithoutRef } from 'react';
 import { StyledWrapper } from './styles';
 
 type CenterProps = ComponentPropsWithoutRef<'div'> & {
-  inline: boolean;
+  inline?: boolean;
+  gap?: number;
 };
 
-export const Center = ({ children, inline = false, ...otherProps }: CenterProps) => {
+export const Center = ({ children, inline, gap, ...otherProps }: CenterProps) => {
   return (
-    <StyledWrapper $inline={inline} {...otherProps}>
+    <StyledWrapper $inline={inline} $gap={gap} {...otherProps}>
       {children}
     </StyledWrapper>
   );
