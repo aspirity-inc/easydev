4c4f8e0827116b3ad6bc889c8c59ae756a66b8f4
Slava Dmitriev <vdmitriev@aspirity.com>
2023-09-12 12:18:56 +0700

    change tests

--- src/core/Badge/Badge.test.tsx
+++ src/core/Badge/Badge.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { darken } from 'polished';
-import { describe, beforeEach, expect, test } from 'vitest';
+import { expect, test } from 'vitest';
 
 import { EasydevProvider } from '@core/Theme';
 import { lightPalette } from '@core/Theme/themePalette';
@@ -9,38 +8,6 @@ import { Badge } from '.';
 import { defaultColorShades } from './styles';
 
 const mockBadgeText = 'Badge text';
-
-describe('Badge default', () => {
-  beforeEach(() => {
-    render(
-      <EasydevProvider>
-        <Badge>{mockBadgeText}</Badge>
-      </EasydevProvider>
-    );
-  });
-
-  test('Should be in document', () => {
-    expect(screen.getByText(mockBadgeText).parentNode).toBeInTheDocument();
-  });
-
-  test('width should be auto', () => {
-    expect(screen.getByText(mockBadgeText).parentNode).toHaveStyleRule('width', 'auto');
-  });
-
-  test('background color should be success lightPalette.success', () => {
-    expect(screen.getByText(mockBadgeText).parentNode).toHaveStyleRule(
-      'background-color',
-      lightPalette.success[defaultColorShades.bgColor]
-    );
-  });
-
-  test('color should be success lightPalette.success', () => {
-    expect(screen.getByText(mockBadgeText).parentNode).toHaveStyleRule(
-      'color',
-      lightPalette.success[defaultColorShades.textColor]
-    );
-  });
-});
 
 test('background-color and color should be error lightPalette.error', () => {
   render(
@@ -58,37 +25,14 @@ test('background-color and color should be error lightPalette.error', () => {
   );
 });
 
-test('color should be custom', () => {
-  render(
-    <EasydevProvider>
-      <Badge color="#efefef">{mockBadgeText}</Badge>
-    </EasydevProvider>
-  );
-  expect(screen.getByText(mockBadgeText).parentNode).toHaveStyleRule('color', darken(0.3, '#efefef'));
-  expect(screen.getByText(mockBadgeText).parentNode).toHaveStyleRule('background-color', '#efefef');
-});
-
 test('color and textColor should be custom', () => {
   render(
     <EasydevProvider>
-      <Badge color="#efefef" textColor="#ededed">
-        {mockBadgeText}
-      </Badge>
+      <Badge color="#efefef" textColor="#ededed">{mockBadgeText}</Badge>
     </EasydevProvider>
   );
-
-  expect(screen.getByText(mockBadgeText).parentNode).toHaveStyleRule('color', '#ededed');
+  expect(screen.getByText(mockBadgeText).parentNode).toHaveStyleRule('color',  '#ededed');
   expect(screen.getByText(mockBadgeText).parentNode).toHaveStyleRule('background-color', '#efefef');
-});
-
-test('Should be in the document with chosen size', () => {
-  render(
-    <EasydevProvider>
-      <Badge size="lg">{mockBadgeText}</Badge>
-    </EasydevProvider>
-  );
-
-  expect(screen.getByText(mockBadgeText).parentNode).toBeInTheDocument();
 });
 
 test('width should be 100%', () => {
@@ -97,6 +41,5 @@ test('width should be 100%', () => {
       <Badge fullWidth>{mockBadgeText}</Badge>
     </EasydevProvider>
   );
-
   expect(screen.getByText(mockBadgeText).parentNode).toHaveStyleRule('width', '100%');
 });
