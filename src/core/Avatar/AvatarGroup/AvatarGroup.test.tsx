3856ac23d5b1545ce80066c2024ac905baaa2601
Slava Dmitriev <vdmitriev@aspirity.com>
2023-09-14 14:51:47 +0700

    join tests

--- src/core/Avatar/AvatarGroup/AvatarGroup.test.tsx
+++ src/core/Avatar/AvatarGroup/AvatarGroup.test.tsx
@@ -9,7 +9,7 @@ import { sizes } from '../styles';
 
 const avatarGroupClassName = 'avatar-group';
 
-test('Should be correct AvatarGroup padding-left (depends from 1st child size)', () => {
+test('Should be correct AvatarGroup (padding-left) and (margin-left between children) (depends from 1st child size)', () => {
   const { container } = render(
     <EasydevProvider>
       <AvatarGroup className={avatarGroupClassName}>
@@ -22,6 +22,15 @@ test('Should be correct AvatarGroup padding-left (depends from 1st child size)',
 
   const avatarGroupElement = container.getElementsByClassName(avatarGroupClassName)[0];
   expect(avatarGroupElement).toHaveStyleRule('padding-left', `${Number.parseInt(sizes['sm'].avatar) / 2}px`);
+
+  expect(avatarGroupElement.childNodes[0]).toHaveStyleRule(
+    'margin-left',
+    `-${Number.parseInt(sizes['sm'].avatar) / 2}px`
+  );
+  expect(avatarGroupElement.childNodes[1]).toHaveStyleRule(
+    'margin-left',
+    `-${Number.parseInt(sizes['sm'].avatar) / 2}px`
+  );
 });
 
 test('Should be correct AvatarGroup custom padding-left and every avatar should have size of first element', () => {
@@ -42,27 +51,6 @@ test('Should be correct AvatarGroup custom padding-left and every avatar should 
 
   expect(avatarGroupElement.childNodes[1]).toHaveStyleRule('width', sizes['sm'].avatar);
   expect(avatarGroupElement.childNodes[1]).toHaveStyleRule('height', sizes['sm'].avatar);
-});
-
-test('Should be correct margin-left between children (depends from 1st child size)', () => {
-  const { container } = render(
-    <EasydevProvider>
-      <AvatarGroup className={avatarGroupClassName}>
-        <Avatar size="sm" />
-        <Avatar size="xxl" />
-      </AvatarGroup>
-    </EasydevProvider>
-  );
-
-  const avatarGroupElement = container.getElementsByClassName(avatarGroupClassName)[0];
-  expect(avatarGroupElement.childNodes[0]).toHaveStyleRule(
-    'margin-left',
-    `-${Number.parseInt(sizes['sm'].avatar) / 2}px`
-  );
-  expect(avatarGroupElement.childNodes[1]).toHaveStyleRule(
-    'margin-left',
-    `-${Number.parseInt(sizes['sm'].avatar) / 2}px`
-  );
 });
 
 test('Should be correct custom margin-left between children', () => {
