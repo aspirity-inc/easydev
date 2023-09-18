392a88ec0ae1f8b52679fbbe5427bb7999afe6e1
Slava Dmitriev <vdmitriev@aspirity.com>
2023-09-18 18:06:00 +0700

    add card tests

--- src/core/Card/Card.test.tsx
+++ src/core/Card/Card.test.tsx
@@ -1,16 +1,65 @@
-import { render } from '@testing-library/react';
-import { test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { expect, test } from 'vitest';
 
-import { EasydevProvider } from '@core/Theme';
+import { EasydevProvider, THEMES } from '@core/Theme';
+import { lightPalette } from '@core/Theme/themePalette';
 
 import { Card } from '.';
 
-const mockBadgeText = 'Badge text';
+const mockCardText = 'Card text';
 
-test('background-color and color should be error lightPalette.error', () => {
-  render(
+test('variant shadow', () => {
+  const { container } = render(
     <EasydevProvider>
-      <Card>{mockBadgeText}</Card>
+      <Card variant="shadow">{mockCardText}</Card>
     </EasydevProvider>
   );
+
+  expect(container.querySelector('div')).toHaveStyleRule(
+    'box-shadow',
+    (THEMES.light.shadows.gray as string).split(':')[1].trim()
+  );
+});
+
+test('variant outline', () => {
+  const { container } = render(
+    <EasydevProvider>
+      <Card variant="outline">{mockCardText}</Card>
+    </EasydevProvider>
+  );
+
+  expect(container.querySelector('div')).toHaveStyleRule('border', `1px solid ${lightPalette.surface['400']}`);
+});
+
+test('direction', () => {
+  const { container } = render(
+    <EasydevProvider>
+      <Card direction="column">{mockCardText}</Card>
+    </EasydevProvider>
+  );
+
+  expect(container.querySelector('div')).toHaveStyleRule('flex-wrap', 'wrap');
+});
+
+test('media and borderMedia', () => {
+  render(
+    <EasydevProvider>
+      <Card direction="column" media={<div>media</div>} borderedMedia>
+        {mockCardText}
+      </Card>
+    </EasydevProvider>
+  );
+	
+  expect(screen.getByText('media')).toBeInTheDocument();
+  expect(screen.getByText('media').parentNode).toHaveStyleRule('padding', '16px 16px 0 16px');
+});
+
+test('maxWidth', () => {
+  const { container } = render(
+    <EasydevProvider>
+      <Card maxWidth="120px">{mockCardText}</Card>
+    </EasydevProvider>
+  );
+
+  expect(container.querySelector('div')).toHaveStyleRule('max-width', '120px');
 });
