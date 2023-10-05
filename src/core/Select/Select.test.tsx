870b5c96230d0c65fe4c845fc1186bc062d226c9
Slava Dmitriev <vdmitriev@aspirity.com>
2023-10-05 12:01:22 +0700

    EASY-1138 add select tests

--- src/core/Select/Select.test.tsx
+++ src/core/Select/Select.test.tsx
@@ -18,23 +18,18 @@ const options: OptionType[] = [
   { value: 'vanilla2', label: 'Vanilla2', isDisabled: true },
 ];
 
-test('', async () => {
+test('default select', async () => {
   const { container } = render(
     <EasydevProvider>
-      <Select options={options} placeholder="SELECT" />
+      <Select options={options} placeholder="SELECT" rounded />
     </EasydevProvider>
   );
 
-  // expect(document.body).toMatchSnapshot();
-
-  // const selectElement = screen.getByText('SELECT').parentNode as Element;
-
-  // await userEvent.click(selectElement);
+  expect(screen.getByText('SELECT')).toBeInTheDocument();
+  expect(screen.queryByText(options[0].label)).not.toBeInTheDocument();
 
   const openSelectListElement = container.querySelector('.react-select__indicator') as Element;
   await userEvent.click(openSelectListElement);
-  // expect(container.querySelector('.react-select__indicator')).toBeInTheDocument();
-  // expect(document.body).toMatchSnapshot();
 
   const selectMenuElement = container.querySelector('.react-select__menu') as Element;
   expect(selectMenuElement).toBeInTheDocument();
@@ -43,7 +38,118 @@ test('', async () => {
     expect(screen.getByText(option.label)).toBeInTheDocument();
   });
 
-  // expect(document.body).toMatchSnapshot();
-  // const modalClose = screen.getByText(mockModalText).parentNode;
-  // expect(modalClose).toHaveStyleRule('opacity', '0');
+  await userEvent.click(screen.getByText(options[0].label));
+  expect(screen.queryByText('SELECT')).not.toBeInTheDocument();
+  expect(screen.getByText(options[0].label)).toBeInTheDocument();
+
+  expect(container.querySelector('div')).toHaveStyleRule('border-radius', '40px', {
+    modifier: '.react-select__control',
+  });
+});
+
+test('noOptionsMessage', async () => {
+  const noOptionsMessage = 'options list is empty';
+
+  const { container } = render(
+    <EasydevProvider>
+      <Select options={[]} placeholder="SELECT" rounded noOptionsMessage={() => noOptionsMessage} />
+    </EasydevProvider>
+  );
+
+  const openSelectListElement = container.querySelector('.react-select__indicator') as Element;
+  await userEvent.click(openSelectListElement);
+
+  expect(screen.getByText(noOptionsMessage)).toBeInTheDocument();
+});
+
+test('value', async () => {
+  render(
+    <EasydevProvider>
+      <Select options={options} value={options[0]} />
+    </EasydevProvider>
+  );
+
+  expect(screen.getByText(options[0].label)).toBeInTheDocument();
+});
+
+test('icons', async () => {
+  const ControlIcon = () => <div className="material-symbols-rounded">nutrition</div>;
+  const ArrowDown = () => <div className="material-symbols-rounded">keyboard_arrow_down</div>;
+  const SelectedIcon = () => <div className="material-symbols-rounded">priority</div>;
+
+  const { container } = render(
+    <EasydevProvider>
+      <Select
+        options={options}
+        dropdownIcon={<ArrowDown />}
+        selectedIcon={<SelectedIcon />}
+        controlIcon={<ControlIcon />}
+      />
+    </EasydevProvider>
+  );
+
+  expect(screen.getByText('nutrition')).toBeInTheDocument();
+  expect(screen.getByText('keyboard_arrow_down')).toBeInTheDocument();
+
+  const openSelectListElement = container.querySelector('.react-select__indicator') as Element;
+  await userEvent.click(openSelectListElement);
+
+  expect(screen.getAllByText('priority')[0]).toBeInTheDocument();
+});
+
+test('isMulti', async () => {
+  const { container } = render(
+    <EasydevProvider>
+      <Select options={options} isMulti={true} hideSelectedOptions={false} />
+    </EasydevProvider>
+  );
+
+  const openSelectListElement = container.querySelector('.react-select__indicator') as Element;
+  await userEvent.click(openSelectListElement);
+
+  await userEvent.click(container.querySelectorAll('.react-select__option')[0]);
+  await userEvent.click(openSelectListElement);
+
+  await userEvent.click(container.querySelectorAll('.react-select__option')[1]);
+  await userEvent.click(openSelectListElement);
+
+  expect(container.querySelectorAll('.react-select__option--is-selected').length).toBe(2);
+});
+
+test('clearButtonText', async () => {
+  const clearButtonText = 'delete';
+
+  render(
+    <EasydevProvider>
+      <Select options={options} isMulti value={[options[0], options[1]]} clearButtonText={clearButtonText} />
+    </EasydevProvider>
+  );
+
+  expect(screen.getByText(clearButtonText)).toBeInTheDocument();
+});
+
+test('search', async () => {
+  const noOptionsMessage = 'Not found';
+
+  const { container } = render(
+    <EasydevProvider>
+      <Select options={options} isMulti isSearchable noOptionsMessage={() => noOptionsMessage} />
+    </EasydevProvider>
+  );
+
+  await userEvent.type(container.querySelector('input') as Element, 'Cho');
+  expect(container.querySelector('.react-select__menu-list')?.childNodes.length).toBe(3);
+
+  await userEvent.type(container.querySelector('input') as Element, 'Cat');
+  expect(screen.getByText(noOptionsMessage)).toBeInTheDocument();
+});
+
+test('selectType', async () => {
+  render(
+    <EasydevProvider>
+      <Select options={options} selectType="async" />
+    </EasydevProvider>
+  );
+
+  expect(screen.getByText('search').className).toBe('material-symbols-rounded');
 });
