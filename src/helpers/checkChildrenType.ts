2658d8466d73c1b1301261d5eeb6e97d43e06738
Pisklova Marina <pisklovama@gmail.com>
2023-10-10 15:54:40 +0500

    Delete files

--- src/helpers/checkChildrenType.ts
+++
@@ -1,6 +0,0 @@
-import type { ReactPortal, ReactElement, JSXElementConstructor } from 'react';
-
-export const checkChildrenType = (
-  child: ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>>,
-  type: string
-) => (child as any)?.type?.__docgenInfo?.displayName === type;
