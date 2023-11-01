c6555a0e0bc6013018d8a8426b9f714ef0e902a1
Slava Dmitriev <vdmitriev@aspirity.com>
2023-11-01 12:13:44 +0700

    EASY-1157 change tests

--- src/core/Controls/Toggle/Toggle.test.tsx
+++ src/core/Controls/Toggle/Toggle.test.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { expect, test, vi } from 'vitest';
@@ -6,6 +8,7 @@ import { EasydevProvider, THEMES } from '@core/Theme';
 import { lightPalette } from '@core/Theme/themePalette';
 
 import { Toggle } from '.';
+import { StyledToggleInner } from './styles';
 import { getControlColor } from '../styles';
 
 test('toggle', async () => {
@@ -50,17 +53,61 @@ test('disabled', () => {
   );
 });
 
-test('isDayNightMode', async () => {
+test('toggleBackground, toggleInnerBackground', async () => {
+  const dayToggleBackground = "url('/assets/toggle/day.svg')";
+  const nightToggleBackground = "url('/assets/toggle/night.svg')";
+
+  const dayToggleInnerBackground = '#ffc700';
+  const nightToggleInnerBackground = "url('/assets/toggle/night-span.svg')";
+
+  const Component = () => {
+    const [checked, setChecked] = useState(false);
+
+    const [toggleBackground, setToggleBackground] = useState(checked ? dayToggleBackground : nightToggleBackground);
+    const [toggleInnerBackground, setToggleInnerBackground] = useState(
+      checked ? dayToggleInnerBackground : nightToggleInnerBackground
+    );
+
+    const onChange = () => setChecked((prevState) => !prevState);
+
+    useEffect(() => {
+      if (checked) {
+        setToggleBackground(dayToggleBackground);
+        setToggleInnerBackground(dayToggleInnerBackground);
+      } else {
+        setToggleBackground(nightToggleBackground);
+        setToggleInnerBackground(nightToggleInnerBackground);
+      }
+    }, [checked]);
+
+    return (
+      <Toggle
+        checked={checked}
+        onChange={onChange}
+        toggleBackground={toggleBackground}
+        toggleInnerBackground={toggleInnerBackground}
+        toggleWrapProps={{ style: { width: 48 } }}
+        toggleInnerProps={{ style: { translate: checked ? '150% -50%' : '' } }}
+      />
+    );
+  };
   const { container } = render(
     <EasydevProvider>
-      <Toggle isDayNightMode defaultChecked={false} />
+      <Component />
     </EasydevProvider>
   );
 
   const toggleWrapContainer = container.querySelector('.easy_toggle-wrap');
-  expect(toggleWrapContainer).toHaveStyleRule('width', '48px');
-  expect(toggleWrapContainer).toHaveStyleRule('background', "url('/assets/toggle/night.svg')");
+  expect(toggleWrapContainer).toHaveStyle('width: 48px');
+  expect(toggleWrapContainer).toHaveStyleRule('background', nightToggleBackground);
+
+  expect(toggleWrapContainer).toHaveStyleRule('background', nightToggleInnerBackground, {
+    modifier: StyledToggleInner.toString(),
+  });
 
   await userEvent.click(container.querySelector('input') as Element);
-  expect(toggleWrapContainer).toHaveStyleRule('background', "url('/assets/toggle/day.svg')");
+  expect(toggleWrapContainer).toHaveStyleRule('background', dayToggleBackground);
+	expect(toggleWrapContainer).toHaveStyleRule('background', dayToggleInnerBackground, {
+    modifier: StyledToggleInner.toString(),
+  });
 });
