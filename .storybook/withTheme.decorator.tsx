fb14a071fdebbde303e5802f6a638c880462e0b3
Slava Dmitriev <vdmitriev@aspirity.com>
2023-11-02 13:49:06 +0700

    EASY-1177 add global styles to context

--- .storybook/withTheme.decorator.tsx
+++ .storybook/withTheme.decorator.tsx
@@ -1,19 +1,5 @@
-import { createGlobalStyle, DefaultTheme } from 'styled-components';
 import { EasydevProvider, THEMES } from '../src';
 import { StoryContext, StoryFn } from '@storybook/react';
-
-const GlobalStyles = createGlobalStyle<{ theme?: DefaultTheme }>`
-  html, body {
-    background-color: ${({ theme }) => theme.colors.background};
-    color: ${({ theme }) => (theme.type === 'light' ? theme.colors.text : theme.colors.surface['50'])};
-    font-family: "Roboto Flex", sans-serif;
-		scrollbar-gutter: stable;
-  }
-
-  * {
-    box-sizing: border-box;
-  }
-`;
 
 export const withTheme = (Story: StoryFn, context: StoryContext) => {
   const { backgrounds } = context.globals;
@@ -21,7 +7,6 @@ export const withTheme = (Story: StoryFn, context: StoryContext) => {
 
   return (
     <EasydevProvider theme={theme} defaultStyledInjection>
-      <GlobalStyles />
       <Story />
     </EasydevProvider>
   );
